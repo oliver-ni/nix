efcd52ec01c2b88257643743d18bca3206205605
Oliver Ni <oliver.ni@gmail.com>
2023-02-08 20:34:37 -0800

    Only open nvim-tree on directories

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -30,6 +30,17 @@ in
       }
     ];
 
+    initExtra = ''
+      bindkey "^U" backward-kill-line
+
+      autoload -U up-line-or-beginning-search
+      autoload -U down-line-or-beginning-search
+      zle -N up-line-or-beginning-search
+      zle -N down-line-or-beginning-search
+      bindkey "''${terminfo[kcuu1]}" up-line-or-beginning-search # Up
+      bindkey "''${terminfo[kcud1]}" down-line-or-beginning-search # Down
+    '';
+
     shellAliases = {
       python = "python3";
       pip = "pip3";
