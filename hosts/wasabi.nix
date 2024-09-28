00db6079dda6df24d08e00157d41e9dc5e042ed1
Oliver Ni <oliverni@berkeley.edu>
2024-09-28 13:57:43 -0700

    add openssh on wasabi

--- hosts/wasabi.nix
+++ hosts/wasabi.nix
@@ -56,6 +56,8 @@
     extraGroups = [ "wheel" "networkmanager" ];
   };
 
+  services.openssh.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
